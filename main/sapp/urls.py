bfe7c5a784a44da3f3319498bafb9a250ee509a6
Kath De Leon <163301849+kathdeleon18@users.noreply.github.com>
2024-10-04 23:24:43 +0800

    Update urls.py

--- main/sapp/urls.py
+++ main/sapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.cart, name='cart'),
     path('portfolio/', views.portfolio, name='portfolio'),
     path('services/', views.services, name='services'),
 ]
